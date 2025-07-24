787f49bb374310fb8a8fbf625589354622de323c
Rohit Kalgude <rohitkalgude860@gmail.com>
2025-07-24 09:17:29 +0530

    first commit

---
+++ currency.js
@@ -0,0 +1,5 @@
+const Converter = document.getElementById("converter");
+const fromCurrency = document.getElementById("from-currency");
+const toCurrency = document.getElementById("to-currency");
+const amoutInput = document.getElementById("amount");
+const Result = document.getElementById("amount");
