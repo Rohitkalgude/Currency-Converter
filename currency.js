57e2cf1d067503eb5e2476a4c0cdebf8c622e429
Rohit Kalgude <rohitkalgude860@gmail.com>
2025-07-24 09:35:29 +0530

    update

--- currency.js
+++ currency.js
@@ -3,3 +3,26 @@ const fromCurrency = document.getElementById("from-currency");
 const toCurrency = document.getElementById("to-currency");
 const amoutInput = document.getElementById("amount");
 const Result = document.getElementById("amount");
+
+window.addEventListener("load", fetchCurrencies);
+Converter.addEventListener("submit", Convertercurrency);
+
+async function fetchCurrencies() {
+  const response = await fetch(
+    "https://api.exchangerate-api.com/v4/latest/USD"
+  );
+  const data = await response.json();
+  console.log(data);
+
+  const currecyOptions = Object.keys(data.rates);
+  console.log(currecyOptions);
+
+  currecyOptions.forEach((currency) => {
+    const option1 = document.createElement("option");
+    option1.value = currency;
+    option1.textContent = currency;
+    fromCurrency.appendChild(option1);
+  });
+}
+
+function Convertercurrency() {}
